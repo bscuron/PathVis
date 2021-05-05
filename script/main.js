5e579cb1a942d8e4ef7f11e647086f0873e18836
Ben Scuron <bscuron19@gmail.com>
2021-05-04 21:02:59 -0400

    DFS implemented

--- script/main.js
+++ script/main.js
@@ -4,10 +4,14 @@ let grid = [];
 let defaultRows = 20;
 let defaultCols = 20;
 
+// render queue for path visuals
 let pathRenderQueue = [];
 let visitedRenderQueue = [];
 
+//frame rate mod factor ex (rameCount % speed == 0)
 let speed = 1;
+
+//decides which algorithm to use for pathfinding
 let algorithm = 1;
 
 let executionTime = '---';
@@ -34,6 +38,7 @@ function draw(){
 function drawVisual(algo){
     let showAmount = null;
 
+    //depending on algorithm you may want to show chunks of the queue at one time
     if(algo == 1){
         showAmount = 4;
     }else if(algo == 2){
@@ -133,6 +138,7 @@ function drawGrid(){
             }
 
             rect(j * xstep, i * ystep, xstep, ystep);
+
             pop();
         }
     }
@@ -234,6 +240,7 @@ function startSearch(){
 
     //dfs
     else if(algorithm == 2){
+        dfs(grid, getStartNode());
     }
 
     //dijkstra
@@ -334,6 +341,65 @@ function bfs(){
         current = current.parent;
     }
 }
+
+function dfs(){
+
+    let finishNode = getFinishNode();
+    let current = null;
+    let stack = [getStartNode()];
+
+    while(stack.length > 0){
+        current = stack.pop();
+
+        if(current.equals(finishNode)){
+            current = current.parent;
+            while(current.parent != null){
+                pathRenderQueue.push(current);
+                current = current.parent;
+            }
+            break;
+        }
+
+        if(!current.visited){
+            current.visited = true;
+
+            //add to the render queue except for the start node
+            if(current.value != 1)
+                visitedRenderQueue.push(current);
+
+            //north
+            if(current.y > 0 && !grid[current.y-1][current.x].visited && grid[current.y-1][current.x].value != 3){
+                let neighbor = grid[current.y-1][current.x];
+                neighbor.parent = current;
+                stack.push(neighbor);
+            }
+
+            //south
+            if(current.y < rows-1 && !grid[current.y+1][current.x].visited && grid[current.y+1][current.x].value != 3){
+                let neighbor = grid[current.y+1][current.x];
+                neighbor.parent = current;
+                stack.push(neighbor);
+            }
+
+            //east
+            if(current.x < cols-1 && !grid[current.y][current.x+1].visited && grid[current.y][current.x+1].value != 3){
+                let neighbor = grid[current.y][current.x+1];
+                neighbor.parent = current;
+                stack.push(neighbor);
+            }
+
+            //west
+            if(current.x > 0 && !grid[current.y][current.x-1].visited && grid[current.y][current.x-1].value != 3){
+                let neighbor = grid[current.y][current.x-1];
+                neighbor.parent = current;
+                stack.push(neighbor);
+            }
+        }
+    }
+
+    return current;
+}
+
 
 function generateMaze(){
     initGrid();
